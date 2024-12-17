32e003d29f257363501b008c0b78e26e6c066249
Paul Ohl <paul.lv.ohl@gmail.com>
2024-12-17 16:19:23 +0100

    feat: wrote a simple back to send data to

---
+++ contact_back/src/main.js
@@ -0,0 +1,39 @@
+const { Schema, default: mongoose } = require('mongoose');
+const express = require('express');
+
+const ContactDataSchema = new Schema({
+  email: String,
+  role: String,
+  restOfData: String,
+});
+
+const ContactDataModel = mongoose.model('ContactData', ContactDataSchema);
+
+async function handleContactDataUpload(req, res) {
+  const { email, role } = req.body;
+
+  const newContactData = new ContactDataModel({
+    email,
+    role,
+    restOfData: JSON.stringify(req.body),
+  });
+  const savedContactData = await newContactData.save();
+  res.json(savedContactData);
+}
+
+async function main() {
+  const mongo_uri = process.env.MONGO_URI;
+  if (!mongo_uri) {
+    throw new Error('MONGO_URI is not defined');
+  }
+  await mongoose.connect(mongo_uri);
+
+  const app = express();
+  app.use(express.json());
+  app.post('/contact', handleContactDataUpload);
+  app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+  });
+}
+
+main().catch((err) => console.error(err));
